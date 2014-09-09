c1a0f5e23e99e947e2c998df0938d946688e29fe
rogilmore <thatrickgilmore@gmail.com>
2014-09-09 16:18:42 -0400

    Changes in response to Weinstein email.

--- plot-code.R
+++ plot-code.R
@@ -2,18 +2,20 @@
 
 fig5.df = read.csv("weinstein-neutral-density-three-way-2014-09-09.csv", header = TRUE)
 
-require( ggplot2 )
+library( ggplot2 )
+library( grid ) # for arrow function
 
 p = ggplot( data=fig5.df, mapping=aes( effect, threshold, stimulus, eye ) )
 
 # See http://sape.inf.usi.ch/quick-reference/ggplot2/geom_linerange
-p + geom_linerange( data=fig5.df, mapping = aes( x=effect, ymin=ci.lower, ymax=ci.upper, colour=stimulus, linetype=eye ), width=0.2, size=1) +
-  geom_point(fill="white", size=4, shape=21) +
+p +  
+  geom_linerange( data=fig5.df, mapping = aes( x=effect, ymin=ci.lower, ymax=ci.upper, linetype=eye ), size=1.5) +
+  geom_point(mapping=aes( shape=stimulus ), fill="white", size=5 ) +
   geom_text( aes( x=7.5, y=0.1, label="p=.041"), size=5 ) +
   geom_segment( data=fig5.df, aes( x=7.5, y=(threshold[7]+threshold[8])/2, xend=7.5, yend=threshold[7]), width=0.2, 
                 arrow = arrow( length=unit(0.2,"cm")), show_guide=F ) +
   geom_segment(data = fig5.df, aes( x=7.5, y=(threshold[7]+threshold[8])/2, xend=7.5, yend=threshold[8], width=0.2 ),
                arrow=arrow(length=unit(0.2,"cm")), show_guide=F ) +
-  xlab("Interaction Effect") +
+  xlab("Stimulus x Filter x Diagnosis") +
   ylab("Threshold") +
   ggtitle("Thresholds by diagnostic group, stimulus, and filter")
