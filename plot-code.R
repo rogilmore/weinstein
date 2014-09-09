20a6cc7f5999b1b0e748e1bd9747cab4a5dc7475
rogilmore <thatrickgilmore@gmail.com>
2014-09-09 11:43:46 -0400

    fix label text size...again.

--- plot-code.R
+++ plot-code.R
@@ -5,12 +5,11 @@ fig5.df = read.csv("weinstein-neutral-density-three-way-2014-09-09.csv", header 
 require( ggplot2 )
 
 p = ggplot( data=fig5.df, mapping=aes( effect, threshold, stimulus, eye ) )
-# p + geom_bar( stat="identity" )
 
 # See http://sape.inf.usi.ch/quick-reference/ggplot2/geom_linerange
 p + geom_linerange( data=fig5.df, mapping = aes( x=effect, ymin=ci.lower, ymax=ci.upper, colour=stimulus, linetype=eye ), width=0.2, size=1) +
   geom_point(fill="white", size=4, shape=21) +
-  geom_text( aes( x=7.5, y=0.1, label="p=.041"), size=2 ) +
+  geom_text( aes( x=7.5, y=0.1, label="p=.041"), size=5 ) +
   geom_segment( data=fig5.df, aes( x=7.5, y=(threshold[7]+threshold[8])/2, xend=7.5, yend=threshold[7]), width=0.2, 
                 arrow = arrow( length=unit(0.2,"cm")), show_guide=F ) +
   geom_segment(data = fig5.df, aes( x=7.5, y=(threshold[7]+threshold[8])/2, xend=7.5, yend=threshold[8], width=0.2 ),
